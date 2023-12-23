f3738280c0bd417f7ac8f06e002964ad0d65f1fe
Eduardo Alves <eduardoalvesmelo.2018@gmail.com>
2023-12-23 11:31:25 -0400

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$B0Ef411oxUKIRr3M4dDbEe2YKuYYV7PEnZmmhVUMkuPdxbFYUYVV.');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$B0Ef411oxUKIRr3M4dDbEe2YKuYYV7PEnZmmhVUMkuPdxbFYUYVV.');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
